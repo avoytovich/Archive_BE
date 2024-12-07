65649fb6587083af82278c45ea5a740f6ae5f72e
avoytovich <levanwork@gmail.com>
2024-12-07 16:47:06 +0100

    add: delete

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,22 @@
 const { upload } = require('./../helper/uploading');
+const { archiveController } = require('./../controllers');
 
-const { 
-  archiveController
-} = require('./../controllers');
+module.exports = (app) => {
+  // Test route for verification
+  app.get('/test', (req, res) => res.status(200).send({
+    message: 'Welcome'
+  }));
 
-module.exports =
-  (app) => {
-    app.get('/test', (req, res) => res.status(200).send({
-      message: 'Welcome'
-    }));
+  // Route to create a new archive (uploading)
+  app.post('/create', upload.single('archive'), archiveController.create);
 
-    app.post('/create', upload.single('archive'), archiveController.create);
-    app.get('/archives', archiveController.getAll);
-  };
+  // Route to get all archives (with pagination and optional title filter)
+  app.get('/archives', archiveController.getAll);
+
+  // Route to delete a single archive by ID
+  app.delete('/archives/:id', archiveController.delete);
+
+  // Route for bulk delete (accepts an array of IDs)
+  app.post('/archives/bulk-delete', archiveController.bulkDelete);
+};
+
